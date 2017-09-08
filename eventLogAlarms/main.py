a024909de7c372b39f2a4b8a4baa88618dc7a1ed
Rayzian <zhouxiaoxi0516@sohu.com>
2017-09-08 18:11:19 +0800

    修改代码

--- eventLogAlarms/main.py
+++ eventLogAlarms/main.py
@@ -60,44 +60,53 @@ class EventAlarm(object):
         if xml_dict:
             return xml_dict
 
-    # def getLogFile(self, file_name, file_path):
-    #     file_list = []
-    #     for path, dir, file in os.walk(file_path):
-    #         if file == file_name:
-    #             file_list = [path + "/" + i for i in file]
-    #
-    #     return file_list
-
     def parseLogFile(self, file_path, xml):
         path, flag = self.checkFileType(path=file_path)
         with open(path, "r") as f:
             for temp in f:
-                filterkey = xml.keys()[0]
-                if temp and str(filterkey) in temp:
-                    for key in xml[filterkey].keys():
-                        if str(key) == "yinbi":
-                            index = r'yinbi:(\S+)'
-                            pattern = re.compile(pattern=index)
-                        else:
-                            index = r'%s:(\S+?);' % str(key)
-                            pattern = re.compile(pattern=index)
-                        value = re.findall(pattern=pattern, string=temp)
-                        if value and value[0] > xml[filterkey][key]:
-                            warn = {
-                                key: value[0]
-                            }
-                            data = {
-                                "filterkey": filterkey,
-                                "message": temp,
-                                "index": {key: value[0]}
-                            }
-                            # self.insertLogData(data=data)
-                            self.sendLogData(text=temp, warn=warn, xml=xml_dict, filterkey=filterkey, key=key)
-                            warn = {}
+                # filterkey = xml.keys()[0]
+                for filterkey in xml.keys():
+                    if temp and str(filterkey) in temp:
+                        if str(filterkey) != "wupin":
+                            for key in xml[filterkey].keys():
+                                transkey = str(key) + ":"
+                                if transkey == "yinbi:":
+                                    index = r'yinbi:(\S+)'
+                                    pattern = re.compile(pattern=index)
+                                else:
+                                    index = r'%s:(\S+?);' % str(key)
+                                    pattern = re.compile(pattern=index)
+                                value = re.findall(pattern=pattern, string=temp)
+                                if value and int(max(value)) > int((xml[filterkey][key])):
+                                    # warn = {
+                                    #     key: max(value)
+                                    # } }
+                                    log = {key: max(value)}
+                                    self.insertLogData(temp=temp, filterkey=xml[filterkey], log=log)
+                                    # self.sendLogData(text=temp, warn=warn, xml=xml_dict, filterkey=filterkey, key=key)
+                                    # warn = {}
+
+                        elif str(filterkey) == "wupin":
+                            pattern = re.compile(r'wupin:(\S+?);')
+                            value = re.findall(pattern=pattern, string=temp)
+                            if value:
+                                match_split = value[0].strip().split("|")
+                                match_dict = {x.split(",")[0]: x.split(",")[1] for x in match_split}
+                            for key in xml["wupin"].keys():
+                                if key in match_dict.keys():
+                                    if int(match_dict[key]) > int(xml["wupin"][key]):
+                                        # warn = {
+                                        #     key: max(value)
+                                        # }
+                                        log = {key: match_dict[key]}
+                                        self.insertLogData(temp=temp, filterkey=xml[filterkey], log=log)
+                                        # self.sendLogData(text=temp, warn=warn, xml=xml_dict, filterkey=filterkey, key=key)
+                                        # warn = {}=
+
             if flag:
                 os.remove(path)
-        # except Exception, e:
-        #     print e
+                # except Exception, e:
+                #     print e
 
     def checkFileType(self, path):
         if path.endswith(".gz"):
@@ -113,27 +122,32 @@ class EventAlarm(object):
         decompress_path = path[:-3]
         return gz.decompress(gzFile=path, dst=decompress_path)
 
-    def insertLogData(self, data):
+    def insertLogData(self, **kwargs):
+        data = {
+            "log": kwargs["log"],
+            "filterkey": kwargs["filterkey"],
+            "message": kwargs["temp"]
+        }
         self.db["alarmLog"].insert_one(dict(data))
 
-    def sendLogData(self, text, warn, xml, filterkey, key):
-        # try:
-        url = 'https://oapi.dingtalk.com/robot/send?access_token=45f5f3675c7752a245324a79209c9d814aceb80e5d63a8d041e34fc3c4611baf'
-        heards = {
-            "Content-Type": "application/json ;charset=utf-8 "
-        }
-        string_textMsg = {
-            "msgtype": "text",
-            "text": {
-                "content": {
-                            "text": text,
-                            "xml": xml[filterkey][key],
-                            warn.keys()[0]: warn.values()[0]
-                }
-            }
-        }
-        string_textMsg = json.dumps(string_textMsg)
-        requests.post(url, data=string_textMsg, headers=heards)
+        # def sendLogData(self, text, warn, xml, filterkey, key):
+        #     # try:
+        #     url = 'https://oapi.dingtalk.com/robot/send?access_token=45f5f3675c7752a245324a79209c9d814aceb80e5d63a8d041e34fc3c4611baf'
+        #     heards = {
+        #         "Content-Type": "application/json ;charset=utf-8 "
+        #     }
+        #     string_textMsg = {
+        #         "msgtype": "text",
+        #         "text": {
+        #             "content": {
+        #                         "text": text,
+        #                         "xml": xml[filterkey][key],
+        #                         warn.keys()[0]: warn.values()[0]
+        #             }
+        #         }
+        #     }
+        #     string_textMsg = json.dumps(string_textMsg)
+        #     requests.post(url, data=string_textMsg, headers=heards)
         # except Exception, e:
         #     print e
 
@@ -142,11 +156,10 @@ if __name__ == '__main__':
     # try:
     # config, _ = genParserClient()
     # folder_path = config.path
-    file_path = r'/home/zhouxiaoxi/Desktop/eventAlarm/game.log.2017-09-07-09'
+    # file_path = r'/home/zhouxiaoxi/Desktop/eventAlarm/game.log.2017-09-07-09'
+    file_path = r'/home/zhouxiaoxi/Desktop/eventAlarm/new 2.txt'
     event = EventAlarm()
     xml_dict = event.parseXML()
     event.parseLogFile(file_path=file_path, xml=xml_dict)
     # except Exception, e:
     #     print e
-
-
