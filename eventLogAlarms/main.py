038728191e011ea6c58f568d1d0d2c43feee85a3
Rayzian <zhouxiaoxi0516@sohu.com>
2017-09-11 00:20:27 +0800

    添加方法注释

--- eventLogAlarms/main.py
+++ eventLogAlarms/main.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
+"""
+@author zhouxiaoxi
+@简乐互动
 
+"""
 import gzip
 import requests
 import json
@@ -13,12 +17,21 @@ from argUtil import genParserClient
 
 
 class _GZipTool(object):
+    """
+    解压.gz文件
+    """
     def __init__(self, bufSize):
         self.bufSize = bufSize
         self.fin = None
         self.fout = None
 
     def decompress(self, gzFile, dst):
+        """
+        解压.gz文件
+        :param gzFile: 压缩文件路径
+        :param dst: 解压后文件路径
+        :return: 解压后文件路径
+        """
         print "start to decompress: ", gzFile
         self.fin = gzip.open(gzFile, 'rb')
         self.fout = open(dst, 'wb')
@@ -27,6 +40,10 @@ class _GZipTool(object):
         return dst
 
     def __in2out(self):
+        """
+        解压文件
+        :return: None
+        """
         while True:
             buf = self.fin.read(self.bufSize)
             if len(buf) < 1:
@@ -42,6 +59,10 @@ class EventAlarm(object):
         self.db = self.client["eventAlarm"]
 
     def parseXML(self):
+        """
+        解析xml文件
+        :return: 标签属性键值对
+        """
         xml_dict = {}
 
         DOMTree = xml.dom.minidom.parse("index.xml")
@@ -61,6 +82,12 @@ class EventAlarm(object):
             return xml_dict
 
     def parseLogFile(self, file_path, xml):
+        """
+        解析日志文件
+        :param file_path: 日志文件路径
+        :param xml: xml文件
+        :return: None
+        """
         path, flag = self.checkFileType(path=file_path)
         with open(path, "r") as f:
             for temp in f:
@@ -101,7 +128,6 @@ class EventAlarm(object):
                                         log = {key: match_dict[key]}
                                         self.insertLogData(temp=temp, filterkey=xml[filterkey], log=log)
                                         # self.sendLogData(text=temp, warn=warn, xml=xml_dict, filterkey=filterkey, key=key)
-                                        # warn = {}=
 
             if flag:
                 os.remove(path)
@@ -109,6 +135,11 @@ class EventAlarm(object):
                 #     print e
 
     def checkFileType(self, path):
+        """
+        检查文件类型，是否为.gz格式的压缩文件
+        :param path: 文件路径
+        :return: 文件路径和是否为被解压过的标记
+        """
         if path.endswith(".gz"):
             dst = self.decompassFile(path=path)
             decompass = True
@@ -118,11 +149,21 @@ class EventAlarm(object):
             return path, decompass
 
     def decompassFile(self, path):
+        """
+        解压文件
+        :param path: 文件路径
+        :return: 返回解压后的文件路径
+        """
         gz = _GZipTool(bufSize=8192)
         decompress_path = path[:-3]
         return gz.decompress(gzFile=path, dst=decompress_path)
 
     def insertLogData(self, **kwargs):
+        """
+        向mongodb插入数据
+        :param kwargs: 参数键值对
+        :return: None
+        """
         data = {
             "log": kwargs["log"],
             "filterkey": kwargs["filterkey"],
@@ -130,26 +171,35 @@ class EventAlarm(object):
         }
         self.db["alarmLog"].insert_one(dict(data))
 
-        # def sendLogData(self, text, warn, xml, filterkey, key):
-        #     # try:
-        #     url = 'https://oapi.dingtalk.com/robot/send?access_token=45f5f3675c7752a245324a79209c9d814aceb80e5d63a8d041e34fc3c4611baf'
-        #     heards = {
-        #         "Content-Type": "application/json ;charset=utf-8 "
-        #     }
-        #     string_textMsg = {
-        #         "msgtype": "text",
-        #         "text": {
-        #             "content": {
-        #                         "text": text,
-        #                         "xml": xml[filterkey][key],
-        #                         warn.keys()[0]: warn.values()[0]
-        #             }
-        #         }
-        #     }
-        #     string_textMsg = json.dumps(string_textMsg)
-        #     requests.post(url, data=string_textMsg, headers=heards)
-        # except Exception, e:
-        #     print e
+    def sendLogData(self, text, warn, xml, filterkey, key):
+        """
+        使用钉钉机器人发送告警的日志
+        :param text: 日志内容
+        :param warn: 告警关键字键值对
+        :param xml: XML文件键值对
+        :param filterkey: 过滤信息关键字
+        :param key: 关键字
+        :return: None
+        """
+        try:
+            url = 'https://oapi.dingtalk.com/robot/send?access_token=45f5f3675c7752a245324a79209c9d814aceb80e5d63a8d041e34fc3c4611baf'
+            heards = {
+                "Content-Type": "application/json ;charset=utf-8 "
+            }
+            string_textMsg = {
+                "msgtype": "text",
+                "text": {
+                    "content": {
+                        "text": text,
+                        "xml": xml[filterkey][key],
+                        warn.keys()[0]: warn.values()[0]
+                    }
+                }
+            }
+            string_textMsg = json.dumps(string_textMsg)
+            requests.post(url, data=string_textMsg, headers=heards)
+        except Exception, e:
+            print e
 
 
 if __name__ == '__main__':
